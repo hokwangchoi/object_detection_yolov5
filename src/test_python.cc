079d20bf8ecfc77628c89f9d06f80fe6a68e4c42
choi0330 <choi920330@gmail.com>
2021-01-14 17:26:58 +0100

    testing done

--- src/test_python.cc
+++ src/test_python.cc
@@ -1,15 +1,15 @@
 #include <iostream>
 
-#include <boost/python.hpp>
-// #include <boost/python/numpy.hpp>
+#include <boost/python/numpy.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
+#include <opencv2/opencv.hpp>
 #include <python3.8/Python.h>
 
 namespace bp = boost::python;
-// namespace np = boost::python::numpy;
+namespace np = boost::python::numpy;
 
 std::string parse_python_exception() {
   PyObject *type_ptr = NULL, *value_ptr = NULL, *traceback_ptr = NULL;
@@ -61,6 +61,17 @@ void pythonFriendlyTerminate() {
   std::terminate();
 }
 
+np::ndarray ConvertMatToNDArray(const cv::Mat& mat) {
+  bp::tuple shape = bp::make_tuple(mat.rows, mat.cols, mat.channels());
+  bp::tuple stride = bp::make_tuple(
+      mat.channels() * mat.cols * sizeof(uchar), mat.channels() * sizeof(uchar),
+      sizeof(uchar));
+  np::dtype dt = np::dtype::get_builtin<uchar>();
+  np::ndarray ndImg = np::from_data(mat.data, dt, shape, stride, bp::object());
+
+  return ndImg;
+}
+
 int main() {
   if (!termineAlreadyInfiltrated) {
     std::set_terminate(pythonFriendlyTerminate);
@@ -69,44 +80,43 @@ int main() {
 
   // Initialize the python instance
   Py_Initialize();
+  np::initialize();
 
   wchar_t programname[] = L"yolov5_node.py";
   wchar_t* argv[] = {programname, nullptr};
   PySys_SetArgv(1, argv);
 
-  // np::initialize();
-  // std::vector<float> originalVector = { 1.5, 2.5, 3.5, 4.5, 5.5 };
-  // int sizeOfVector = originalVector.size();
-  // float *originalArray = originalVector.data();
-  // std::cout << "sizeOfArray = " << sizeOfVector << std::endl;
-  // np::dtype arrayType = np::dtype::get_builtin<float>();
-  // np::ndarray py_array = np::from_data(
-  //   originalArray,
-  //   arrayType,
-  //   bp::make_tuple(1, sizeOfVector),
-  //   bp::make_tuple(sizeof(float) * 0, sizeof(float) * 1),
-  //   bp::object());
-
   try {
     bp::object main = bp::import("__main__");
     bp::object globals = main.attr("__dict__");
+
     // Run a simple string (verify the python version).
     bp::exec(
         "import sys\n"
         "print(sys.version)",
         globals);
-    bp::exec("import random", globals);
-    bp::object rand = bp::eval("random.random()", globals);
-    std::cout << bp::extract<double>(rand) << std::endl;
+
     bp::object module = bp::import("detect");
     bp::object detector_class = module.attr("DetectorYoloV5");
     bp::object detector_instance = detector_class(0.25, 0.45);
     std::string image_path =
         "/hokwang/data/bags/ghost_bag_cam0/frame000000.png";
-    cv::Mat img = cv::imread(image_path, cv::IMREAD_COLOR);
-    bp::object result = detector_instance.attr("detect")();
-    // const auto& nd_array = bp::extract<np::ndarray>(result);
-    // std::cout <<  << std::endl;
+    cv::Mat mat = cv::imread(image_path, cv::IMREAD_COLOR);
+    bp::object result =
+        detector_instance.attr("detect")(ConvertMatToNDArray(mat));
+
+    np::ndarray nd_array = bp::extract<bp::numpy::ndarray>(result);
+    std::cout << "py_array: " << bp::extract<char const*>(bp::str(nd_array))
+              << std::endl;
+
+    int input_size = nd_array.shape(0) * nd_array.shape(1);
+    double* input_ptr = reinterpret_cast<double*>(nd_array.get_data());
+    std::vector<double> v(input_size);
+    for (int i = 0; i < input_size; ++i) {
+      v[i] = *(input_ptr + i);
+      std::cout << v[i] << " ";
+    }
+    std::cout << std::endl;
   } catch (boost::python::error_already_set const&) {
     std::cout << "Exception in Python:" << parse_python_exception()
               << std::endl;
